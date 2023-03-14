fb1654a28c6d25e011aa4bb507b73bff50c00030
Hatice SÖNMEZ <htcsonmez428@gmail.com>
2023-03-14 12:22:11 +0300

    more examples added

--- independent_two-sample_t-test.py
+++ independent_two-sample_t-test.py
@@ -136,3 +136,90 @@ print("Test Stat = %.4f, p-value = %.4f" % (test_stat, pvalue)) # p-value = 0.00
 test_stat, pvalue = stats.mannwhitneyu(df[df["Progress"] > 75]["Rating"],
                                        df[df["Progress"] < 25]["Rating"])
 print("Test Stat = %.4f, p-value = %.4f" % (test_stat, pvalue)) # p-value = 0.0000 => H0 REDDEDİLİR.
+
+
+
+
+
+
+
+
+
+# Sigara içen grup ile içmeyenlerin nefeslerini tutma süreleri arasında ist. ol. anl. bir fark var mı?
+# H0: M1 = M2
+# H1: M1 != M2
+data = pd.read_excel("datasets/smoke_breath.xlsx")
+data.head()
+data.groupby("Sigara")["Zaman"].mean()
+smoker = data[data["Sigara"] == "Evet"]
+non_smoker = data[data["Sigara"] == "Hayır"]
+# Normallik Varsayımı Testi
+# H0: Veriler normal dağılım göstermektedir.
+# H1: Veriler normal dağılım göstermemektedir.
+norm_smoker = stats.shapiro(smoker["Zaman"])
+norm_non_smoker = stats.shapiro(non_smoker["Zaman"])
+print(f"Smoker pvalue= {norm_smoker[1]}\nNon smoker pvalue= {norm_non_smoker[1]}")
+# Smoker pvalue= 0.0089 , Non smoker pvalue= 0.0603  --> Smoker için H0 Red.
+test_stat, pvalue = stats.mannwhitneyu(smoker["Zaman"], non_smoker["Zaman"])
+print("Test Stat = %.4f, p-value = %.4f" % (test_stat, pvalue))
+# p-value = 0.3514 --> H0 REDDEDİLEMEZ.
+
+
+
+
+
+
+
+
+# Bir içeçek firması ürettiği yeni aromalı içeçek ile eski içecek için müşteri tutumları arasında bir fark olup olm. test etmek istiyor.
+# H0: M1 = M2 (Eski içecek ile yeni içecek müşteri tutumları arasında anl. fark yoktur)
+# H1: M1 != M2 (Fark vardır)
+data = pd.read_excel("datasets/drink.xlsx")
+data.head()
+data.groupby("Grup")["Tutum"].mean()
+old = data[data["Grup"] == "Eski"]["Tutum"]
+new = data[data["Grup"] == "Yeni"]["Tutum"]
+# Normallik Varsayımı Testi
+norm_old = stats.shapiro(old)
+norm_new = stats.shapiro(new)
+print(f"Old pvalue= {norm_old[1]}\nNew pvalue= {norm_new[1]}")
+# Old pvalue= 0.9838, New pvalue= 0.8389 --> H0 REDDEDİLEMEZ. Veriler normal dağılım göstermektedir.
+# Varyans Homojenliği Testi
+var = stats.bartlett(old, new)
+print(var)
+# BartlettResult(statistic=0.24784539637091021, pvalue=0.6185963027262229) --> H0 REDDEDİLEMEZ. Varyanslar homojendir.
+# Parametrik Hipotez testi
+test_stat, pvalue = stats.ttest_ind(old, new)
+print("Test Stat = %.4f, p-value = %.4f" % (test_stat, pvalue))
+# Test Stat = -2.1768, p-value = 0.0431 --> H0 REDDEDİLİR. Tutumlar arasında anlamlı fark vardır.
+
+
+
+
+
+
+
+
+# Bir ML projesine yatırım yapılmış. Ürettiği tahminler neticesinde oluşan gelir ile eski sistemin ürettiği gelirler
+# karşılaştırılıp anlamlı farklılık olup olmadığı test edilmek isteniyor.
+# Model geliştirilmiş ve web sitesine entegre edilmiş.
+# Site kullanıcıları belirli bir kurala göre ikiye bölünmüş olsun.
+# A grubu eski, B grubu yeni sistem
+# Gelir anlamında anlamlı bir iş yapılıp yapılmadığı test edilmek isteniyor.
+# H0: M1 = M2
+# H1: M1 != M2
+A = [30, 27, 21, 27, 29, 30, 20, 20, 27, 32, 35, 22, 24, 23, 25,
+    27, 23, 27, 23, 25, 21, 18, 24, 26, 33, 26, 27, 28, 19, 25]
+B = [37, 39, 31, 31, 34, 38, 30, 36, 29, 28, 38, 28, 37, 37, 30,
+    32, 31, 31, 27, 32, 33, 33, 33, 31, 32, 33, 26, 32, 33, 29]
+import numpy as np
+np.mean(A) # 25.46
+np.mean(B) #  32.36
+# Normallik Kontrolü
+stats.shapiro(A) # pvalue=0.79628
+stats.shapiro(B) # pvalue=0.24584
+# Varyans Kontrolü
+stats.levene(A, B) # pvalue=0.2964124900636569
+# Hipotez Testi
+test_stat, pvalue = stats.ttest_ind(A, B, equal_var=True)
+print("Test Stat = %.4f, p-value = %.4f" % (test_stat, pvalue)) # Test Stat = -7.0287, p-value = 0.0000
